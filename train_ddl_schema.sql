965e7159c6f3704eb55952f481ddbe823f582c3b
dicksor112801 <rdickson1128@gmail.com>
2025-04-17 12:34:14 -0400

    Update and rename inital_table_setup to train_ddl_schema.sql

--- train_ddl_schema.sql
+++ train_ddl_schema.sql
@@ -1,13 +1,15 @@
-//rename the column in transactions
-ALTER TABLE train_transactions_table
-RENAME COLUMN "TransactionID" TO "Identity_TransactionID"
+--DDL statments and formatting from importing data
 
-//makes transactions 
+--rename the column in transactions
+ALTER TABLE train_transactions_table
+RENAME COLUMN "TransactionID" TO "Identity_TransactionID";
+
+--makes transactions from identity a foregin key for transactions
 ALTER TABLE train_transactions_table
 ADD CONSTRAINT "Identity_TransactionID"
 FOREIGN KEY ("Identity_TransactionID")
 REFERENCES train_identity_table("TransactionID");
 
-//makes TransactionID a primary key
+--makes TransactionID a primary key
 ALTER TABLE train_identity_table
 ADD PRIMARY KEY ("TransactionID");
