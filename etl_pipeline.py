2abfdde10a0827c520b5f2ba7264becbcdc58f10
dicksor112801 <rdickson1128@gmail.com>
2025-04-17 17:51:07 -0400

    Update etl_pipeline.py

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sqlalchemy import create_engine
-import mysql.connector as sql
 
 #replace "username" with database username (mine is just default "postgres")
 #replace "password" with database password (mine is just "password")
@@ -10,9 +9,11 @@ import mysql.connector as sql
 #  I would reccommend running a small set of a dataset first, and seeing that the table shows up once it is complete.
 #  Loading these whole tables took a long time for my machine, around 20-30mins FYI
 
-engine = create_engine('postgresql://username:password@localhost:XXXX/databaseName')
-connection = sql.connect(host='localhost', port='XXXX',
-        database="databaseName", user='username', password='password')
+# Create SQLAlchemy engine for PostgreSQL
+engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5432/FinalProject')
 
-df = pd.read_csv('test_identity.csv') #Name of the csv file. I used the test_identity and the test_transactions for starters
-df.to_sql('test_identity', connection) #This would become the name of the table in the database in postAdmin
+# Read the CSV
+df = pd.read_csv('train_transaction.csv')
+
+# Write to SQL table
+df.to_sql('train_transaction', con=engine, if_exists='replace', index=False)
